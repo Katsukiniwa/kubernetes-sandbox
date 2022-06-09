c646afbc92e1dd5414490a168c4de6a8e2faf333
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-06-09 22:05:38 +0900

    create src

---
+++ src/productService/app.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from 'express'
+
+const app: Application = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', async (_req: Request, res: Response) => {
+  return res.status(200).send({
+    message: 'Hello World!',
+  })
+})
+
+try {
+  app.listen(process.env.PORT, () => {
+    console.log(`dev server running at: http://localhost:${process.env.PORT}/`)
+  })
+} catch (e) {
+  if (e instanceof Error) {
+    console.error(e.message)
+  }
+}
