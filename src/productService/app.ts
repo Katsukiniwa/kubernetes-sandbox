521d5fde35860db1b17bf708f9ffec09aa902400
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-06-11 18:59:02 +0900

    add products endpoint

--- src/productService/app.ts
+++ src/productService/app.ts
@@ -6,8 +6,19 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', async (_req: Request, res: Response) => {
-  return res.status(200).send({
-    message: 'Hello World!',
+  return res.status(200).json({
+    message: 'Hello World!!!',
+  })
+})
+
+app.get('/products', async (_req: Request, res: Response) => {
+  const frontendHost = process.env.FRONTEND_HOST || ''
+  res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3000', frontendHost, "*"])
+  return res.json({
+    products: [
+      { id: '1', name: 'test 001' },
+      { id: '2', name: 'test 002' },
+    ]
   })
 })
 
@@ -19,7 +30,7 @@ app.get('/api/health', async (_req: Request, res: Response) => {
 
 try {
   app.listen(process.env.PORT, () => {
-    console.log(`dev server running at: http://localhost:${process.env.PORT}/`)
+    console.log(`dev server running at: http://localhost:${process.env.PORT}`)
   })
 } catch (e) {
   if (e instanceof Error) {
